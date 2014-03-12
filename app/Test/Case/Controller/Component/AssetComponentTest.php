dad61d4bfffd0cd93e24362f27eeb1e5cba4c9d2
NEKOGET <info@nekoget.net>
2014-03-12 17:12:49 +0900

    不要コード除去　文末改行追加　フォーマット修正

--- app/Test/Case/Controller/Component/AssetComponentTest.php
+++ app/Test/Case/Controller/Component/AssetComponentTest.php
@@ -12,31 +12,14 @@ class TestAssetComponentTestController extends Controller {
 
 class AssetComponentTest extends CakeTestCase {
 
-	public $Controller = null;
-
-/**
- * Fixtures
- * @var array
- */
-	public $fixtures = array(
-		'app.session'
-	);
-
-	public function setUp() {
-		parent::setUp();
-	}
-
-	public function tearDown() {
-		parent::tearDown();
-	}
-
 	public function testIsThemeBootstrapMinCss() {
 		$App = new TestAssetComponentTestController;
 		$Collection = new ComponentCollection();
-		$AssetComponent = new AssetComponent( $Collection );
+		$AssetComponent = new AssetComponent($Collection);
 		$App->theme = "amelia";
-		$this->assertEquals(true, $AssetComponent->isThemeBootstrapMinCss( $App ));
+		$this->assertEquals(true, $AssetComponent->isThemeBootstrapMinCss($App));
 		$App->theme = "none";//存在しないテーマ
 		$this->assertEquals(false, $AssetComponent->isThemeBootstrapMinCss($App));
 	}
 }
+
