101f0f616c64808c8e3fef1c1b9fdc4fdf5c5f2f
topaz2 <topaz2.3333@gmail.com>
2014-04-16 16:32:21 +0900

    Fixed syntax

--- tools/chef/site-cookbooks/netcommons/recipes/default.rb
+++ tools/chef/site-cookbooks/netcommons/recipes/default.rb
@@ -3,32 +3,32 @@
 # Recipe:: default
 #
 
-%w{ postgresql-server-dev-all }.each do |pkg|
+%w( postgresql-server-dev-all ).each do |pkg|
   package pkg do
     action [:install]
   end
 end
 
-include_recipe "boilerplate_php"
+include_recipe 'boilerplate_php'
 
 # Install gem packages
-execute "install netcommons related gem packages" do
+execute 'install netcommons related gem packages' do
   command "cd #{node[:boilerplate][:app_root]}; gemrat pg --no-version"
   only_if { ::File.exists?("#{node[:boilerplate][:app_root]}/Gemfile")}
 end
 
 ## Setup apache
-include_recipe "apache2"
+include_recipe 'apache2'
 
-apache_module "rewrite" do
+apache_module 'rewrite' do
   enable true
 end
 
 ## Setup postgresql
-include_recipe "postgresql::server"
+include_recipe 'postgresql::server'
 
 template "#{node[:postgresql][:dir]}/pg_hba.conf" do
-  source "postgresql/pg_hba.conf"
+  source 'postgresql/pg_hba.conf'
   notifies :restart, 'service[postgresql]'
 end
 
