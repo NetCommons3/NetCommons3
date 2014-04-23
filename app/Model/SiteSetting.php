3d1233983181da35f212fd45513971ea1426f1e3
宮川 貴子 <info@nekoget.net>
2014-04-23 11:27:29 +0900

    __constract追加

--- app/Model/SiteSetting.php
+++ app/Model/SiteSetting.php
@@ -40,6 +40,18 @@ class SiteSetting extends AppModel {
 	);
 
 /**
+ * construct
+ * @param integer|string|array $id Set this ID for this model on startup, can also be an array of options, see above.
+ * @param string $table Name of database table to use.
+ * @param string $ds DataSource connection name.
+ * @return  void
+ * @since   v 3.0.0.0
+ */
+	public function __construct($id = false, $table = null, $ds = null) {
+		parent::__construct($id, $table, $ds);
+	}
+
+/**
  * サイトに設定されているテーマを返す
  * @author Takako Miyagawa <nekoget@gmail.com>
  * @return string or null
