992b33f1fc8b70988bfad8dfd21f01c16463b01c
topaz2 <topaz2.3333@gmail.com>
2014-04-23 16:25:14 +0900

    Test commit how to access axis value

--- tools/build/app/cakephp/pre.sh
+++ tools/build/app/cakephp/pre.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -x
 
-mysql -utest -ptest -e 'DROP DATABASE IF EXISTS test_nc3; CREATE DATABASE test_nc3';
+if [ "$DB" = 'mysql' ]; then mysql -e 'DROP DATABASE IF EXISTS test_nc3; CREATE DATABASE test_nc3;'; fi
+if [ "$DB" = 'pgsql' ]; then psql -c 'CREATE DATABASE test_nc3;' -U postgres; fi
+if [ "$DB" = 'pgsql' ]; then psql -c 'CREATE SCHEMA test2;' -U postgres -d test_nc3; fi
+if [ "$DB" = 'pgsql' ]; then psql -c 'CREATE SCHEMA test3;' -U postgres -d test_nc3; fi
+#mysql -utest -ptest -e 'DROP DATABASE IF EXISTS test_nc3; CREATE DATABASE test_nc3';
+echo $DB
+echo ${DB}
 composer install
 chmod -R 777 ./app/tmp
 mkdir -p build/logs
