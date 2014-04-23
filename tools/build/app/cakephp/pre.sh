78b15c65b5c1b1f9be734e06df14d34f15b3d42f
topaz2 <topaz2.3333@gmail.com>
2014-04-23 15:57:48 +0900

    Added ant build

--- tools/build/app/cakephp/pre.sh
+++ tools/build/app/cakephp/pre.sh
@@ -5,4 +5,11 @@ composer install
 chmod -R 777 ./app/tmp
 mkdir -p build/logs
 cp app/Config/database.php.$DB app/Config/database.php
-for p in `cat app/Config/vendors.txt`; do export IGNORE_PLUGINS=$IGNORE_PLUGINS,$WORKSPACE/app/Plugin/$p; export IGNORE_PLUGINS_OPTS="$IGNORE_PLUGINS_OPTS --exclude app/Plugin/$p"; done
+
+for p in `cat app/Config/vendors.txt`
+do
+  export IGNORE_PLUGINS=$IGNORE_PLUGINS,$WORKSPACE/app/Plugin/$p
+  export IGNORE_PLUGINS_OPTS="$IGNORE_PLUGINS_OPTS --exclude app/Plugin/$p"
+done
+
+ant build-parallel
