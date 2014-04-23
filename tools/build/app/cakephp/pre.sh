41e91c44388e12a0968e3b5dd686113e7741fc97
topaz2 <topaz2.3333@gmail.com>
2014-04-23 17:57:13 +0900

    Splitted one command into two

--- tools/build/app/cakephp/pre.sh
+++ tools/build/app/cakephp/pre.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -ex
 
 if [ "$DB" = 'mysql' ]; then mysql -utest -ptest -e 'DROP DATABASE IF EXISTS test_nc3; CREATE DATABASE test_nc3;'; fi
-if [ "$DB" = 'postgresql' ]; then psql -c 'DROP DATABASE IF EXISTS test_nc3; CREATE DATABASE test_nc3;' -U postgres; fi
+if [ "$DB" = 'postgresql' ]; then
+  psql -c 'DROP DATABASE IF EXISTS test_nc3;' -U postgres
+  psql -c 'CREATE DATABASE test_nc3;' -U postgres
+fi
 if [ "$DB" = 'postgresql' ]; then psql -c 'CREATE SCHEMA test2;' -U postgres -d test_nc3; fi
 if [ "$DB" = 'postgresql' ]; then psql -c 'CREATE SCHEMA test3;' -U postgres -d test_nc3; fi
 composer install
