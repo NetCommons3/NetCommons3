4bbeec46087e4479dae80e09c6919ecaad69403c
topaz2 <topaz2.3333@gmail.com>
2014-04-24 10:23:18 +0900

    Revert version pinning back

--- tools/build/vagrant/pre.sh
+++ tools/build/vagrant/pre.sh
@@ -3,9 +3,9 @@
 vagrant --version
 
 mkdir -p build/logs
-vagrant plugin install vagrant-berkshelf
+# vagrant plugin install vagrant-berkshelf
 # vagrant plugin uninstall vagrant-berkshelf
-# vagrant plugin install vagrant-berkshelf --plugin-version=1.3.7
+vagrant plugin install vagrant-berkshelf --plugin-version=1.3.7
 # vagrant plugin install vagrant-vbguest
 # vagrant plugin uninstall vagrant-vbguest
 vagrant plugin install vagrant-hostmanager
