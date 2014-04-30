2a23f4a84baf8b17f30161c3a0ec283eeb28a61f
topaz2 <topaz2.3333@gmail.com>
2014-04-30 16:05:52 +0900

    Added dependencies

--- tools/chef/site-cookbooks/netcommons/metadata.rb
+++ tools/chef/site-cookbooks/netcommons/metadata.rb
@@ -1,8 +1,8 @@
-maintainer       "topaz2"
-maintainer_email "topaz2@m0n0m0n0.com"
-license          "NetCommons License"
-description      "Installs/Configures netcommons"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+name 'netcommons'
+maintainer 'topaz2'
+maintainer_email 'topaz2@m0n0m0n0.com'
+license 'NetCommons License'
+description 'Installs/Configures netcommons'
+version '0.1.0'
 
-depends "boilerplate_php"
+depends 'boilerplate_php'
