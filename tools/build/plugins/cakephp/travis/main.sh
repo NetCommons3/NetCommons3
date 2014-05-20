6639b5be79648a80a74121d3c13d0f4eceef3cfc
宮川 貴子 <info@nekoget.net>
2014-05-20 18:42:24 +0900

    javascript test 条件追加

--- tools/build/plugins/cakephp/travis/main.sh
+++ tools/build/plugins/cakephp/travis/main.sh
@@ -5,5 +5,6 @@ phpcs -p --extensions=php,ctp --standard=CakePHP --ignore=app/Config/Migration/,
 phpmd app text ruleset/phpmd.xml --exclude $NETCOMMONS_BUILD_DIR/app/Config/Migration,$IGNORE_PLUGINS
 phpcpd --exclude Test --exclude Config --exclude app/Config/database.php $IGNORE_PLUGINS_OPTS app
 
+if [ -e ./Plugin/$PLUGIN_NAME/JavascriptTest/ ]; then
 ./node_modules/karma/bin/karma  start Plugin/$PLUGIN_NAME/JavascriptTest/travis.karma.conf.js --single-run --browsers PhantomJS
-
+fi
