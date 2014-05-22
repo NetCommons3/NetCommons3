863781fff933daf3a47481359f970446c24e974e
topaz2 <topaz2.3333@gmail.com>
2014-05-22 17:55:23 +0900

    Fixed to use composer update

--- tools/build/app/cakephp/pre.sh
+++ tools/build/app/cakephp/pre.sh
@@ -12,7 +12,8 @@ fi
 if [ "$DB" = 'postgresql' ]; then psql -c 'CREATE SCHEMA test2;' -U postgres -d test_nc3; fi
 if [ "$DB" = 'postgresql' ]; then psql -c 'CREATE SCHEMA test3;' -U postgres -d test_nc3; fi
 
-composer install
+composer self-update
+composer update
 chmod -R 777 ./app/tmp
 mkdir -p build/logs
 cp app/Config/database.php.$DB app/Config/database.php
