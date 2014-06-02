1de741257f056a822c93d991564a42731b4d0b1d
Ryuji Masukawa <masukawa@nii.ac.jp>
2014-06-02 20:29:13 +0900

    Edited travis script shell

--- tools/build/plugins/cakephp/travis/main.sh
+++ tools/build/plugins/cakephp/travis/main.sh
@@ -1,18 +1,28 @@
 #!/bin/bash -ex
 
 ./lib/Cake/Console/cake test $PLUGIN_NAME All$PLUGIN_NAME --stderr
-eval "phpcs -p --extensions=php,ctp --standard=CakePHP --ignore=app/Config/Migration/,app/Config/database.php,$IGNORE_PLUGINS app"
+echo "Running phpcs..."
+phpcs -p --extensions=php,ctp --standard=CakePHP --ignore=app/Config/Migration/,app/Config/database.php,$IGNORE_PLUGINS app
 if [ $? != 0 ]
     then
-        exit 1
+        echo "**** Coding standards failed ****"
+        EXIT=1
 fi
-eval "phpmd app text ruleset/phpmd.xml --exclude $NETCOMMONS_BUILD_DIR/app/Config/Migration,$IGNORE_PLUGINS"
+echo -n "Running phpmd..."
+phpmd app text ruleset/phpmd.xml --exclude $NETCOMMONS_BUILD_DIR/app/Config/Migration,$IGNORE_PLUGINS
 if [ $? != 0 ]
     then
-        exit 1
+        echo "**** Mess detection failed ****"
+        EXIT=1
 fi
+echo -n "Running phpcpd..."
 phpcpd --exclude Test --exclude Config $IGNORE_PLUGINS_OPTS app
-
+if [ $? != 0 ]
+    then
+        echo "**** Copy/Paste detection failed ****"
+        EXIT=1
+fi
 if [ -e ./app/Plugin/$PLUGIN_NAME/JavascriptTest/ ]; then
 ./node_modules/karma/bin/karma  start app/Plugin/$PLUGIN_NAME/JavascriptTest/travis.karma.conf.js --single-run --browsers PhantomJS
 fi
+exit $EXIT
