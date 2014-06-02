157d5d98b14ddea7409b4c43bef22ed1638c07ca
Ryuji Masukawa <masukawa@nii.ac.jp>
2014-06-02 19:22:16 +0900

    Edited travis script shell

--- tools/build/plugins/cakephp/travis/main.sh
+++ tools/build/plugins/cakephp/travis/main.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -ex
 
 ./lib/Cake/Console/cake test $PLUGIN_NAME All$PLUGIN_NAME --stderr
-phpcs -p --extensions=php,ctp --standard=CakePHP --ignore=app/Config/Migration/,app/Config/database.php,$IGNORE_PLUGINS app
-phpmd app text ruleset/phpmd.xml --exclude $NETCOMMONS_BUILD_DIR/app/Config/Migration,$IGNORE_PLUGINS
+eval "phpcs -p --extensions=php,ctp --standard=CakePHP --ignore=app/Config/Migration/,app/Config/database.php,$IGNORE_PLUGINS app"
+if [ $? != 0 ]
+    then
+        exit 1
+fi
+eval "phpmd app text ruleset/phpmd.xml --exclude $NETCOMMONS_BUILD_DIR/app/Config/Migration,$IGNORE_PLUGINS"
+if [ $? != 0 ]
+    then
+        exit 1
+fi
 phpcpd --exclude Test --exclude Config $IGNORE_PLUGINS_OPTS app
 
-if [ -e ./Plugin/$PLUGIN_NAME/JavascriptTest/ ]; then
-./node_modules/karma/bin/karma  start Plugin/$PLUGIN_NAME/JavascriptTest/travis.karma.conf.js --single-run --browsers PhantomJS
+if [ -e ./app/Plugin/$PLUGIN_NAME/JavascriptTest/ ]; then
+./node_modules/karma/bin/karma  start app/Plugin/$PLUGIN_NAME/JavascriptTest/travis.karma.conf.js --single-run --browsers PhantomJS
 fi
