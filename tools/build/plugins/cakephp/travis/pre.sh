809b9574de73cbe13777a7fc3679b5b712aec6a0
topaz2 <topaz2.3333@gmail.com>
2014-06-03 17:25:22 +0900

    Fixed path issue

--- tools/build/plugins/cakephp/travis/pre.sh
+++ tools/build/plugins/cakephp/travis/pre.sh
@@ -28,8 +28,8 @@ cp app/Config/database.php.travis app/Config/database.php
 
 for p in `cat app/Config/vendors.txt`
 do
-  export IGNORE_PLUGINS="$IGNORE_PLUGINS,$TRAVIS_BUILD_DIR/app/Plugin/$p"
-  export IGNORE_PLUGINS_OPTS="$IGNORE_PLUGINS_OPTS --exclude Plugin/$p"
+  export IGNORE_PLUGINS="$IGNORE_PLUGINS,$NETCOMMONS_BUILD_DIR/app/Plugin/$p"
+  export IGNORE_PLUGINS_OPTS="$IGNORE_PLUGINS_OPTS --exclude app/Plugin/$p"
 done
 export IGNORE_PLUGINS=`echo $IGNORE_PLUGINS | cut -c 2-`
 
