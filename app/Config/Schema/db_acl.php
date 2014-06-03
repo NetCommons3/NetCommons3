eca078e1b3b7e2c7bea7279e36ccc9a535aa9d27
topaz2 <topaz2.3333@gmail.com>
2014-06-03 18:18:12 +0900

    Fixed phpmd error

--- app/Config/Schema/db_acl.php
+++ app/Config/Schema/db_acl.php
@@ -16,6 +16,7 @@
  * @package       app.Config.Schema
  * @since         CakePHP(tm) v 0.2.9
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ * @codingStandardsIgnoreFile
  */
 
 /*
--- app/Test/Case/AllTest.php
+++ app/Test/Case/AllTest.php
@@ -1,6 +1,11 @@
 <?php
 class AllTest extends CakeTestSuite {
 
+/**
+ * All test suite
+ *
+ * @author Jun Nishikawa <topaz2@m0n0m0n0.com>
+ */
 	public static function suite() {
 		$suite = new CakeTestSuite('All tests');
 		$suite->addTestDirectoryRecursive(TESTS . 'Case');
