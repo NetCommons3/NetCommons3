988a09d6f99060e619ab7ef56363e72b9b0d02e3
topaz2 <topaz2.3333@gmail.com>
2014-06-04 10:25:54 +0900

    Externalized build scripts

---
+++ tools/build/app/cakephp/travis/main.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -ex
+
+./lib/Cake/Console/cake test app All --stderr --configuration phpunit.xml.dist || exit $?
+phpcs -p --extensions=php --standard=CakePHP --ignore=app/Config/Migration/,app/Config/database.php,$IGNORE_PLUGINS app || exit $?
+phpmd app text ruleset/phpmd.xml --exclude $TRAVIS_BUILD_DIR/app/Config/Migration,$IGNORE_PLUGINS || exit $?
+phpcpd --exclude Test --exclude Config $IGNORE_PLUGINS_OPTS app
+
+exit 0
---
+++ tools/build/app/cakephp/travis/pre.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -ex
+
+composer install
+
+sh -c "if [ '$DB' = 'mysql' ]; then mysql -e 'CREATE DATABASE cakephp_test;'; fi"
+sh -c "if [ '$DB' = 'mysql' ]; then mysql -e 'CREATE DATABASE cakephp_test2;'; fi"
+sh -c "if [ '$DB' = 'mysql' ]; then mysql -e 'CREATE DATABASE cakephp_test3;'; fi"
+sh -c "if [ '$DB' = 'pgsql' ]; then psql -c 'CREATE DATABASE cakephp_test;' -U postgres; fi"
+sh -c "if [ '$DB' = 'pgsql' ]; then psql -c 'CREATE SCHEMA test2;' -U postgres -d cakephp_test; fi"
+sh -c "if [ '$DB' = 'pgsql' ]; then psql -c 'CREATE SCHEMA test3;' -U postgres -d cakephp_test; fi"
+
+chmod -R 777 ./app/tmp
+mkdir -p build/logs
+
+pear config-set auto_discover 1
+pear channel-discover pear.phpunit.de
+pear channel-discover pear.cakephp.org
+pear channel-discover pear.phpmd.org
+pear channel-discover pear.pdepend.org
+pear install --alldeps phpunit/PHPUnit-3.7.32
+pear install --alldeps cakephp/CakePHP_CodeSniffer
+pear install phpmd/PHP_PMD
+pear install phpunit/phpcpd
+
+phpenv rehash
+set +H
+cp app/Config/database.php.travis app/Config/database.php
+
+for p in `cat app/Config/vendors.txt`
+do
+  export IGNORE_PLUGINS="$IGNORE_PLUGINS,$NETCOMMONS_BUILD_DIR/app/Plugin/$p"
+  export IGNORE_PLUGINS_OPTS="$IGNORE_PLUGINS_OPTS --exclude app/Plugin/$p"
+done
+export IGNORE_PLUGINS=`echo $IGNORE_PLUGINS | cut -c 2-`
