e2cebb4d4355f565885207c8bc5195e05cc466d3
topaz2 <topaz2.3333@gmail.com>
2014-06-10 14:22:21 +0900

    Removed exit 0

--- tools/build/app/cakephp/travis/main.sh
+++ tools/build/app/cakephp/travis/main.sh
@@ -4,5 +4,3 @@
 phpcs -p --extensions=php --standard=CakePHP --ignore=app/Config/Migration/,app/Config/database.php,$IGNORE_PLUGINS app || exit $?
 phpmd app text ruleset/phpmd.xml --exclude $TRAVIS_BUILD_DIR/app/Config/Migration,$IGNORE_PLUGINS || exit $?
 phpcpd --exclude Test --exclude Config $IGNORE_PLUGINS_OPTS app
-
-exit 0
