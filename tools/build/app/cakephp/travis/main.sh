210f07357f134ab1d3611abd7726ec0a27c2f984
topaz2 <topaz2.3333@gmail.com>
2014-06-11 16:42:34 +0900

    Added debug code for travis build

--- tools/build/app/cakephp/travis/main.sh
+++ tools/build/app/cakephp/travis/main.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -ex
 
+echo $IGNORE_PLUGINS
+echo '-----------------------'
+echo $IGNORE_PLUGINS_OPTS
+echo '-----------------------'
+echo $TRAVIS_BUILD_DIR
+
 ./lib/Cake/Console/cake test app All --stderr --configuration phpunit.xml.dist || exit $?
 phpcs -p --extensions=php --standard=CakePHP --ignore=app/Config/Migration/,app/Config/database.php,$IGNORE_PLUGINS app || exit $?
 phpmd app text ruleset/phpmd.xml --exclude $TRAVIS_BUILD_DIR/app/Config/Migration,$IGNORE_PLUGINS || exit $?
