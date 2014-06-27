fe96539e6b66e1734902f1fead7a934aad00bee1
topaz2 <topaz2.3333@gmail.com>
2014-06-27 13:26:21 +0900

    Ignored schema dir

--- tools/build/plugins/cakephp/travis/main.sh
+++ tools/build/plugins/cakephp/travis/main.sh
@@ -10,7 +10,7 @@ echo $NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME/Config/Migration
 ls -al $NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME/Config/Migration
 ls -al app/Plugin/$PLUGIN_NAME/Config/Migration
 ls -al app/Plugin/$PLUGIN_NAME/Config/Schema
-phpmd app/Plugin/$PLUGIN_NAME text ruleset/phpmd.xml --exclude $NETCOMMONS_BUILD_DIR/app/Config/Migration,$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME/Config/Migration,$IGNORE_PLUGINS || exit $?
+phpmd app/Plugin/$PLUGIN_NAME text ruleset/phpmd.xml --exclude $NETCOMMONS_BUILD_DIR/app/Config/Migration,$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME/Config/Migration,app/Plugin/$PLUGIN_NAME/Config/Schema,$IGNORE_PLUGINS || exit $?
 phpcpd --exclude Test --exclude Config $IGNORE_PLUGINS_OPTS app/Plugin/$PLUGIN_NAME
 if [ -d ./app/Plugin/$PLUGIN_NAME/JavascriptTest/ ]; then
   ./node_modules/karma/bin/karma start app/Plugin/$PLUGIN_NAME/JavascriptTest/travis.karma.conf.js --single-run --browsers PhantomJS || exit $?
