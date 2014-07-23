a72521798de65177dfe5cef4bdea4731ec415036
topaz2 <topaz2.3333@gmail.com>
2014-07-23 11:58:51 +0900

    Reverted settings for chef solo

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,3 +6,12 @@ validation_client_name   'chef-validator'
 validation_key           '/etc/chef-server/chef-validator.pem'
 chef_server_url          'https://chef.local:443'
 syntax_check_cache_path  'syntax_check_cache'
+
+CHEF_ROOT = 'tools/chef'.freeze
+
+cookbook_path             ["#{CHEF_ROOT}/cookbooks", "#{CHEF_ROOT}/site-cookbooks"]
+node_path                 "#{CHEF_ROOT}/nodes"
+role_path                 "#{CHEF_ROOT}/roles"
+environment_path          "#{CHEF_ROOT}/environments"
+data_bag_path             "#{CHEF_ROOT}/data_bags"
+encrypted_data_bag_secret "#{CHEF_ROOT}/data_bag_key"
