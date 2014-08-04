d3c749a219f19967612f8f6db44a4337a0293860
topaz2 <topaz2.3333@gmail.com>
2014-08-04 16:14:47 +0900

    Split build jobs

--- tools/build/vagrant/pre.sh
+++ tools/build/vagrant/pre.sh
@@ -11,4 +11,3 @@ vagrant plugin install vagrant-berkshelf --plugin-version '>= 2.0.1'
 vagrant plugin install vagrant-hostmanager
 vagrant plugin install vagrant-omnibus
 vagrant destroy --force
-vagrant up
