1569b3942839071894e9a1983e52f97aa80c386f
topaz2 <topaz2.3333@gmail.com>
2014-09-04 12:02:16 +0900

    Fixed phpmd rules path

--- tools/build/plugins/cakephp/travis/pre.sh
+++ tools/build/plugins/cakephp/travis/pre.sh
@@ -32,7 +32,7 @@ if [ "$PLUGIN_NAME" != "Install" ]; then
   cp app/Config/database.php.travis app/Config/database.php
 fi
 cp tools/build/app/cakephp/phpunit.xml.dist .
-sudo wget https://raw.githubusercontent.com/topaz2/chef_boilerplate_php/master/files/default/build/cakephp/phpmd.xml -O /etc/phpmd.xml
+sudo wget https://raw.githubusercontent.com/topaz2/chef_boilerplate_php/master/files/default/build/cakephp/phpmd/rules.xml -O /etc/phpmd/rules.xml
 
 for p in `cat app/Config/vendors.txt`
 do
