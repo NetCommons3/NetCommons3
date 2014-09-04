ac6725d6ca0b11b7af26215178aa0c00494049d6
topaz2 <topaz2.3333@gmail.com>
2014-09-04 15:13:15 +0900

    Excluded app/Config/Schema/schema.php from phpcs

--- tools/build/app/cakephp/travis/main.sh
+++ tools/build/app/cakephp/travis/main.sh
@@ -2,7 +2,7 @@
 
 # php
 ./lib/Cake/Console/cake test app All --stderr --configuration phpunit.xml.dist || exit $?
-phpcs -p --extensions=php --standard=CakePHP --ignore=app/Config/Migration/,app/Config/database.php,$IGNORE_PLUGINS app || exit $?
+phpcs -p --extensions=php --standard=CakePHP --ignore=app/Config/Migration/,app/Config/database.php,app/Config/Schema/schema.php,$IGNORE_PLUGINS app || exit $?
 phpmd app text /etc/phpmd/rules.xml --exclude $TRAVIS_BUILD_DIR/app/Config/Migration,$IGNORE_PLUGINS || exit $?
 phpcpd --exclude Test --exclude Config --exclude Vendor $IGNORE_PLUGINS_OPTS app
 
