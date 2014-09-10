dc093167779af9359b93cc1a2dea1a08be107937
topaz2 <topaz2.3333@gmail.com>
2014-09-10 16:31:22 +0900

    Fixed phpdoc error

--- app/Test/Case/AllTest.php
+++ app/Test/Case/AllTest.php
@@ -2,6 +2,15 @@
 /**
  * All test suite
  */
+
+/**
+ * All test suite
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 class AllTest extends CakeTestSuite {
 
 /**
--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *
+ * Dummy index for app dir
  *
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
@@ -14,5 +14,4 @@
  * @since         CakePHP(tm) v 0.10.0.1076
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
 require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
