9e693705689a5d0894a139eeb0b9d2a7b68e7965
topaz2 <topaz2.3333@gmail.com>
2014-10-23 14:44:40 +0900

    Fix failed builds

--- tools/chef/roles/sdb.rb
+++ tools/chef/roles/sdb.rb
@@ -1,8 +1,8 @@
 name 'sdb'
 description 'mysql slave'
 
-run_list 'recipe[boilerplate::apt_fast]', 'recipe[build-essential]', 'recipe[mysql::server]',
-'recipe[boilerplate::mysql]'
+run_list 'recipe[boilerplate::apt_fast]', 'recipe[build-essential]',
+'recipe[database::mysql]', 'recipe[mysql::server]', 'recipe[boilerplate::mysql]'
 
 override_attributes(
   'mysql' => {
