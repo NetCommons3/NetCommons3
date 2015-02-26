fa6d3a9df70bb2e0d9e5c6fc2379553c5eec538f
topaz2 <topaz2.3333@gmail.com>
2015-02-26 13:57:37 +0900

    Tons of specification changes in progress

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -107,14 +107,14 @@ class AppModel extends Model {
  * @param array $models models to load
  * @return void
  */
-	public function loadModels($models = []) {
-		foreach ($models as $model => $class) {
-			$this->$model = ClassRegistry::init($class);
-			var_dump(ClassRegistry::init($class, true));
-			var_dump(get_class($this->$model));
-			if ($this->$model->useDbConfig !== 'test') {
-				$this->$model->setDataSource('master');
-			}
-		}
-	}
+	/* public function loadModels(array $models = []) { */
+	/* 	foreach ($models as $model => $class) { */
+	/* 		$this->$model = ClassRegistry::init($class); */
+	/* 		var_dump(ClassRegistry::init($class, true)); */
+	/* 		var_dump(get_class($this->$model)); */
+	/* 		if ($this->$model->useDbConfig !== 'test') { */
+	/* 			$this->$model->setDataSource('master'); */
+	/* 		} */
+	/* 	} */
+	/* } */
 }
