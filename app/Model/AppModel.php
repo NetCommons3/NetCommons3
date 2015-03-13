76638177881a383a1a198469c3aadcc7afac944b
topaz2 <topaz2.3333@gmail.com>
2015-03-13 18:10:40 +0900

    Tons of specification changes in progress

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -19,7 +19,8 @@
  * @author        Jun Nishikawa <topaz2@m0n0m0n0.com>
  */
 
-App::uses('Model', 'Model');
+App::uses('NetCommonsAppModel', 'NetCommons.Model');
+
 
 /**
  * Application model for Cake.
@@ -31,90 +32,5 @@ App::uses('Model', 'Model');
  * @SuppressWarnings(PHPMD.NumberOfChildren)
  * @codeCoverageIgnore
  */
-class AppModel extends Model {
-
-/**
- * use behaviors
- *
- * @var array
- */
-	public $actsAs = array(
-		'NetCommons.Trackable',
-	);
-
-/**
- * Validation rules
- *
- * @var array
- */
-	public $validate = array();
-
-/**
- * Constructor. Binds the model's database table to the object.
- *
- * @param bool|int|string|array $id Set this ID for this model on startup,
- * can also be an array of options, see above.
- * @param string $table Name of database table to use.
- * @param string $ds DataSource connection name.
- * @return void
- * @SuppressWarnings(PHPMD.BooleanArgumentFlag)
- */
-	public function __construct($id = false, $table = null, $ds = null) {
-		// If a datasource is set via params, use it and return
-		if ((is_array($id) && isset($id['ds'])) || $ds) {
-			parent::__construct($id, $table, $ds);
-			return;
-		}
-
-		// Use a static variable, to only use one connection per page-call (otherwise we would get a new handle every time a Model is created)
-		static $_useDbConfig;
-		if (!isset($_useDbConfig)) {
-			// Get all available database-configs
-			$sources = ConnectionManager::enumConnectionObjects();
-
-			// Find the slaves we have
-			$slaves = array();
-			foreach ($sources as $name => $values) {
-				unset($values);
-				// Slaves have to be named "slave1", "slave2", etc...
-				if (preg_match('/^slave[0-9]+$/i', $name) == 1) {
-					$slaves[] = $name;
-				}
-			}
-
-			// Randomly use a slave
-			$_useDbConfig = $slaves[rand(0, count($slaves) - 1)];
-		}
-		$this->useDbConfig = $_useDbConfig;
-
-		parent::__construct($id, $table, $ds);
-	}
-
-/**
- * Sets the DataSource to which this model is bound.
- *
- * @param string $dataSource The name of the DataSource, as defined in app/Config/database.php
- * @return void
- * @throws MissingConnectionException
- */
-	public function setDataSource($dataSource = null) {
-		$this->useDbConfig !== 'test' && parent::setDataSource($dataSource);
-	}
-
-/**
- * Load models
- *
- * @param array $models models to load
- * @param string $source data source
- * @return void
- */
-	public function loadModels(array $models = [], $source = 'master') {
-		/* ClassRegistry::flush(); */
-		foreach ($models as $model => $class) {
-			$this->$model = ClassRegistry::init($class, true);
-			if ($this->$model->useDbConfig !== 'test') {
-				$this->$model->setDataSource($source);
-			}
-		}
-	}
+class AppModel extends NetCommonsAppModel {
 }
