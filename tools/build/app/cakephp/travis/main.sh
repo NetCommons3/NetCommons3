4ef01b7185f4b318ebd4e69cff4a630d87977047
topaz2 <topaz2.3333@gmail.com>
2015-05-25 13:13:29 +0900

    Fix cakephp path

--- tools/build/app/cakephp/travis/main.sh
+++ tools/build/app/cakephp/travis/main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
-# php
-./lib/Cake/Console/cake test app All --stderr --configuration phpunit.xml.dist || exit $?
-
 export PATH=$PATH:./vendors/bin
+
+# php
+cake test app All --stderr --configuration phpunit.xml.dist || exit $?
 phpcs -p --extensions=php --standard=./vendors/cakephp/cakephp-codesniffer/CakePHP --ignore=app/Config/Migration/,app/Config/database.php,app/Config/Schema/schema.php,$IGNORE_PLUGINS app || exit $?
 phpmd app text /etc/phpmd/rules.xml --exclude $TRAVIS_BUILD_DIR/app/Config/Migration,$TRAVIS_BUILD_DIR/app/Config/Schema/schema.php,$IGNORE_PLUGINS || exit $?
 phpcpd --exclude Test --exclude Config --exclude Vendor $IGNORE_PLUGINS_OPTS app
