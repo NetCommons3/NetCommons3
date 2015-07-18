dbf55b24471d1d696542177bdf73c44f8c338936
s-nakajima <nakajimashouhei@gmail.com>
2015-07-18 15:32:10 +0000

    Fix composer.json

---
+++ tools/build/plugins/cakephp/travis/environment.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+
+phpenv rehash
+set +H
+
+export PLUGIN_NAME=`basename $TRAVIS_BUILD_DIR`
+
+for p in `cat app/Config/vendors.txt`
+do
+  export IGNORE_PLUGINS="$IGNORE_PLUGINS,$NETCOMMONS_BUILD_DIR/app/Plugin/$p"
+  export IGNORE_PLUGINS_OPTS="$IGNORE_PLUGINS_OPTS --exclude app/Plugin/$p"
+done
+export IGNORE_PLUGINS=`echo $IGNORE_PLUGINS | cut -c 2-`
+
+export PATH=$PATH:./vendors/bin
