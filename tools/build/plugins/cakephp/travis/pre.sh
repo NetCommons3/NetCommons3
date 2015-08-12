2719048466b3c5d603739aa19778df714a8e08fe
s-nakajima <nakajimashouhei@gmail.com>
2015-08-13 07:46:38 +0900

    netcommonsプラグインのcomposer.jsonを自動で読み込むように修正

--- tools/build/plugins/cakephp/travis/pre.sh
+++ tools/build/plugins/cakephp/travis/pre.sh
@@ -8,7 +8,7 @@ export PLUGIN_NAME=`basename $TRAVIS_BUILD_DIR`
 rm composer.json
 rm composer.lock
 sudo wget https://raw.githubusercontent.com/NetCommons3/NetCommons/master/composer.json
-chmod a+w composer.json
+sudo chmod a+w composer.json
 composer require netcommons/net-commons:"@dev"
 composer install
 
