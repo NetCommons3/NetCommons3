2fc038ae8dea98fedf1eadbf83aa8ceab5007cc2
RyujiAMANO <ryuji@ryus.co.jp>
2015-10-29 09:32:42 +0900

    サーバ（PHP)のタイムゾーンをUTCに固定

--- app/Config/core.php
+++ app/Config/core.php
@@ -268,7 +268,7 @@
  * Uncomment this line and correct your server timezone to fix
  * any date & time related errors.
  */
-	//date_default_timezone_set('UTC');
+	date_default_timezone_set('UTC');
 
 /**
  * `Config.timezone` is available in which you can set users' timezone string.
