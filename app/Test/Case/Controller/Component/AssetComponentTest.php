ce55b76ddf78a855d461164b92236304de3500ec
s-nakajima <nakajimashouhei@gmail.com>
2016-03-05 11:14:38 +0900

    SiteManager.SiteSettingに変更

--- app/Test/Case/Controller/Component/AssetComponentTest.php
+++ app/Test/Case/Controller/Component/AssetComponentTest.php
@@ -2,18 +2,23 @@
 /**
  * TestAssetComponentTest
  *
- * @author   Jun Nishikawa <topaz2@m0n0m0n0.com>
- * @link     http://www.netcommons.org NetCommons Project
- * @license  http://www.netcommons.org/license.txt NetCommons License
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
  */
+
 App::uses('Controller', 'Controller');
 App::uses('ComponentCollection', 'Controller');
 App::uses('AssetComponent', 'Controller/Component');
-App::uses('SiteSetting', 'NetCommons.Model');
 
 /**
  * Class TestAssetComponentTestController
  * CompornentをテストするためのController Class
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\Test\Fixture
  */
 class TestAssetComponentTestController extends Controller {
 }
@@ -29,7 +34,7 @@ class TestAssetComponentTestControllerGetSiteTheme extends Controller {
  *
  * @var array
  */
-	public $uses = array("NetCommons.SiteSetting");
+	public $uses = array('SiteManager.SiteSetting');
 }
 
 /**
@@ -44,7 +49,7 @@ class AssetComponentTest extends CakeTestCase {
  * @var array
  */
 	public $fixtures = array(
-		'plugin.net_commons.site_setting',
+		'plugin.site_manager.site_setting',
 		'plugin.users.user',
 	);
 
@@ -57,9 +62,9 @@ class AssetComponentTest extends CakeTestCase {
 		$App = new TestAssetComponentTestController;
 		$Collection = new ComponentCollection();
 		$AssetComponent = new AssetComponent($Collection);
-		$App->theme = "amelia";
+		$App->theme = 'amelia';
 		$this->assertEquals(true, $AssetComponent->isThemeBootstrapMinCss($App));
-		$App->theme = "none";//存在しないテーマ
+		$App->theme = 'none';//存在しないテーマ
 		$this->assertEquals(false, $AssetComponent->isThemeBootstrapMinCss($App));
 	}
 
@@ -74,9 +79,9 @@ class AssetComponentTest extends CakeTestCase {
 		$AssetComponent = new AssetComponent($Collection);
 		//登録されている場合
 		$ck = $AssetComponent->getSiteTheme($App);
-		$this->assertTextEquals("UnitTestTheme", $ck);
+		$this->assertTextEquals('UnitTestTheme', $ck);
 		$App->SiteSetting->delete(2);
-		$this->assertEquals(array(), $App->SiteSetting->find('first', array("conditions" => array('SiteSetting.id' => 2))));
+		$this->assertEquals(array(), $App->SiteSetting->find('first', array('conditions' => array('SiteSetting.id' => 2))));
 		//登録されていなかった場合
 		$ck = $AssetComponent->getSiteTheme($App);
 		$this->assertNull($ck);
