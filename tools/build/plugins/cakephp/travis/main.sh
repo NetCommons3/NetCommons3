78c6abe98a8e7e2b270253efc3e112cbbfeb123e
Shohei Nakajima <nakajimashouhei@gmail.com>
2016-03-12 22:43:28 +0900

    Update main.sh

--- tools/build/plugins/cakephp/travis/main.sh
+++ tools/build/plugins/cakephp/travis/main.sh
@@ -8,6 +8,12 @@ phpcs -p --extensions=php,ctp --standard=./vendors/cakephp/cakephp-codesniffer/C
 phpmd app/Plugin/$PLUGIN_NAME text /etc/phpmd/rules.xml --exclude $NETCOMMONS_BUILD_DIR/app/Config/Migration,$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME/Config/Migration,$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME/Config/Schema,$IGNORE_PLUGINS || exit $?
 phpcpd --exclude Test --exclude Config $IGNORE_PLUGINS_OPTS app/Plugin/$PLUGIN_NAME
 
+# js
+gjslint --strict -x jquery.js,jquery.cookie.js,js_debug_toolbar.js,travis.karma.conf.js,my.karma.conf.js -e jasmine_examples,HtmlPurifier,webroot/components,webroot/js/langs -r app || exit $?
+if [ -d ./app/Plugin/$PLUGIN_NAME/JavascriptTest/ ]; then
+  ./node_modules/karma/bin/karma start app/Plugin/$PLUGIN_NAME/JavascriptTest/travis.karma.conf.js --single-run --browsers PhantomJS || exit $?
+fi
+
 # phpdoc
 LOG=/var/log/phpdoc.log
 sudo touch $LOG
@@ -17,10 +23,3 @@ echo "phpdoc app/Plugin/$PLUGIN_NAME"
 phpdoc parse -d app/Plugin/$PLUGIN_NAME -t $TRAVIS_BUILD_DIR/phpdoc --force --ansi | tee $LOG
 [ `grep -c '\[37;41m' $LOG` -ne 0 ] && cat $LOG && exit 1
 echo "phpdoc no error."
-echo ""
-
-# js
-gjslint --strict -x jquery.js,jquery.cookie.js,js_debug_toolbar.js,travis.karma.conf.js,my.karma.conf.js -e jasmine_examples,HtmlPurifier,webroot/components,webroot/js/langs -r app || exit $?
-if [ -d ./app/Plugin/$PLUGIN_NAME/JavascriptTest/ ]; then
-  ./node_modules/karma/bin/karma start app/Plugin/$PLUGIN_NAME/JavascriptTest/travis.karma.conf.js --single-run --browsers PhantomJS || exit $?
-fi
