a34496b470461dccf89fb8b89af230943173b486
Shohei Nakajima <nakajimashouhei@gmail.com>
2016-04-23 19:03:49 +0900

    Update post.sh

--- tools/build/plugins/cakephp/travis/post.sh
+++ tools/build/plugins/cakephp/travis/post.sh
@@ -13,23 +13,25 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 
   if [ $GH_TOKEN ]; then
     PHP_VERSION="`php --version`"
-    if [ `echo $PHP_VERSION | grep "^PHP 5.5"` ]; then
+    check=`echo $PHP_VERSION | grep "^PHP 5.5"`
+
+    if [ ! "$check" = "" ]; then
       git clone -b gh-pages git://github.com/NetCommons3/NetCommons3Docs $NETCOMMONS_BUILD_DIR/NetCommons3Docs
       cd $NETCOMMONS_BUILD_DIR/NetCommons3Docs
-	
+
       PHPDOC_OPTIONS="$NETCOMMONS_BUILD_DIR/app/Controller/,$NETCOMMONS_BUILD_DIR/app/Lib/,$NETCOMMONS_BUILD_DIR/app/Locale/,$NETCOMMONS_BUILD_DIR/app/Model/,$NETCOMMONS_BUILD_DIR/app/Test/,$NETCOMMONS_BUILD_DIR/app/Vendor/,$NETCOMMONS_BUILD_DIR/app/View/,$NETCOMMONS_BUILD_DIR/app/Plugin/NetCommons/"
-	
+
       if [ -d phpdoc/$PLUGIN_NAME ]; then
         rm -r phpdoc/$PLUGIN_NAME
       fi
       echo "phpdoc $NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME"
       phpdoc run -d "$PHPDOC_OPTIONS,$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME" -t phpdoc/$PLUGIN_NAME -i "*/Test/*" --cache-folder phpdoc/$PLUGIN_NAME/cache --force --ansi --log $LOG2 > $LOG
-	
+
       rm -r phpdoc/$PLUGIN_NAME/cache
-	
+
       git add -A
       git commit -m "Update phpdoc $PLUGIN_NAME"
-	
+
       git push --quiet https://$GH_TOKEN@github.com/NetCommons3/NetCommons3Docs.git 2> /dev/null
     fi
   fi
