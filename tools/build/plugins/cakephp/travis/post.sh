f2ef37bb87ffb510ed50165de3ccdcf6a67f4a39
Shohei Nakajima <nakajimashouhei@gmail.com>
2016-04-23 15:23:13 +0900

    Update post.sh

--- tools/build/plugins/cakephp/travis/post.sh
+++ tools/build/plugins/cakephp/travis/post.sh
@@ -17,22 +17,24 @@ export GIT_COMMITTER_EMAIL=nakajimashouhei@gmail.com
 export GIT_AUTHOR_NAME=s-nakajima
 export GIT_AUTHOR_EMAIL=nakajimashouhei@gmail.com
 
-if [ "$TRAVIS_BRANCH" == "master" -a $GH_TOKEN -a $PLUGIN_NAME ]; then
-  git clone -b gh-pages git://github.com/NetCommons3/NetCommons3Docs $NETCOMMONS_BUILD_DIR/NetCommons3Docs
-  cd $NETCOMMONS_BUILD_DIR/NetCommons3Docs
-
-  PHPDOC_OPTIONS="$NETCOMMONS_BUILD_DIR/app/Controller/,$NETCOMMONS_BUILD_DIR/app/Lib/,$NETCOMMONS_BUILD_DIR/app/Locale/,$NETCOMMONS_BUILD_DIR/app/Model/,$NETCOMMONS_BUILD_DIR/app/Test/,$NETCOMMONS_BUILD_DIR/app/Vendor/,$NETCOMMONS_BUILD_DIR/app/View/,$NETCOMMONS_BUILD_DIR/app/Plugin/NetCommons/"
-
-  if [ -d phpdoc/$PLUGIN_NAME ]; then
-    rm -r phpdoc/$PLUGIN_NAME
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  if [ $GH_TOKEN ]; then
+    git clone -b gh-pages git://github.com/NetCommons3/NetCommons3Docs $NETCOMMONS_BUILD_DIR/NetCommons3Docs
+	  cd $NETCOMMONS_BUILD_DIR/NetCommons3Docs
+	
+	  PHPDOC_OPTIONS="$NETCOMMONS_BUILD_DIR/app/Controller/,$NETCOMMONS_BUILD_DIR/app/Lib/,$NETCOMMONS_BUILD_DIR/app/Locale/,$NETCOMMONS_BUILD_DIR/app/Model/,$NETCOMMONS_BUILD_DIR/app/Test/,$NETCOMMONS_BUILD_DIR/app/Vendor/,$NETCOMMONS_BUILD_DIR/app/View/,$NETCOMMONS_BUILD_DIR/app/Plugin/NetCommons/"
+	
+	  if [ -d phpdoc/$PLUGIN_NAME ]; then
+	    rm -r phpdoc/$PLUGIN_NAME
+	  fi
+	  echo "phpdoc $NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME"
+	  phpdoc run -d "$PHPDOC_OPTIONS,$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME" -t phpdoc/$PLUGIN_NAME -i "$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME/Test/*" --cache-folder phpdoc/$PLUGIN_NAME/cache --force --ansi --log $LOG2 > $LOG
+	
+	  rm -r phpdoc/$PLUGIN_NAME/cache
+	
+	  git add -A
+	  git commit -m "Update phpdoc $PLUGIN_NAME"
+	
+	  git push --quiet https://$GH_TOKEN@github.com/NetCommons3/NetCommons3Docs.git 2> /dev/null
   fi
-  echo "phpdoc $NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME"
-  phpdoc run -d "$PHPDOC_OPTIONS,$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME" -t phpdoc/$PLUGIN_NAME -i "$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME/Test/*" --cache-folder phpdoc/$PLUGIN_NAME/cache --force --ansi --log $LOG2 > $LOG
-
-  rm -r phpdoc/$PLUGIN_NAME/cache
-
-  git add -A
-  git commit -m "Update phpdoc $PLUGIN_NAME"
-
-  git push --quiet https://$GH_TOKEN@github.com/NetCommons3/NetCommons3Docs.git 2> /dev/null
 fi
