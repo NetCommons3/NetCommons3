2f83e85e1af9376a02aeecaa9f9642694117e1fb
Shohei Nakajima <nakajimashouhei@gmail.com>
2016-04-23 17:15:27 +0900

    Update main.sh

--- tools/build/plugins/cakephp/travis/main.sh
+++ tools/build/plugins/cakephp/travis/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-export PATH=$PATH:./vendors/bin
+export PATH=$PATH:$NETCOMMONS_BUILD_DIR/vendors/bin
 
 # php
 app/Console/cake test $PLUGIN_NAME All$PLUGIN_NAME --stderr || exit $?
