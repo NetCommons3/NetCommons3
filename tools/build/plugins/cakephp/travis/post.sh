40a87a660a2be8ad9125741b3d6df942966ee622
Shohei Nakajima <nakajimashouhei@gmail.com>
2016-04-23 15:59:21 +0900

    Update post.sh

--- tools/build/plugins/cakephp/travis/post.sh
+++ tools/build/plugins/cakephp/travis/post.sh
@@ -15,8 +15,8 @@ export GIT_COMMITTER_EMAIL=nakajimashouhei@gmail.com
 export GIT_AUTHOR_NAME=s-nakajima
 export GIT_AUTHOR_EMAIL=nakajimashouhei@gmail.com
 
-PHP_VERSION=`phpenv versions`
-echo $PHP_VERSION
+echo $TRAVIS_BRANCH
+echo $GH_TOKEN
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   if [ $GH_TOKEN ]; then
@@ -27,16 +27,16 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 	
     if [ -d phpdoc/$PLUGIN_NAME ]; then
       rm -r phpdoc/$PLUGIN_NAME
-      fi
-      echo "phpdoc $NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME"
-      phpdoc run -d "$PHPDOC_OPTIONS,$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME" -t phpdoc/$PLUGIN_NAME -i "$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME/Test/*" --cache-folder phpdoc/$PLUGIN_NAME/cache --force --ansi --log $LOG2 > $LOG
+    fi
+    echo "phpdoc $NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME"
+    phpdoc run -d "$PHPDOC_OPTIONS,$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME" -t phpdoc/$PLUGIN_NAME -i "$NETCOMMONS_BUILD_DIR/app/Plugin/$PLUGIN_NAME/Test/*" --cache-folder phpdoc/$PLUGIN_NAME/cache --force --ansi --log $LOG2 > $LOG
 	
-      rm -r phpdoc/$PLUGIN_NAME/cache
+    rm -r phpdoc/$PLUGIN_NAME/cache
 	
-      git add -A
-      git commit -m "Update phpdoc $PLUGIN_NAME"
+    git add -A
+    git commit -m "Update phpdoc $PLUGIN_NAME"
 	
-      git push --quiet https://$GH_TOKEN@github.com/NetCommons3/NetCommons3Docs.git 2> /dev/null
+    git push --quiet https://$GH_TOKEN@github.com/NetCommons3/NetCommons3Docs.git 2> /dev/null
   fi
 fi
 
