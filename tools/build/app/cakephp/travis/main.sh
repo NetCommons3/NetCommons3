47e18cc6f25eafc1ba89cc9d2816464c7c19415f
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-09-30 11:29:08 +0900

    sudo false対応

--- tools/build/app/cakephp/travis/main.sh
+++ tools/build/app/cakephp/travis/main.sh
@@ -5,7 +5,8 @@ export PATH=$PATH:./vendors/bin
 # php
 app/Console/cake test app All --stderr --configuration phpunit.xml.dist || exit $?
 phpcs -p --extensions=php --standard=./vendors/cakephp/cakephp-codesniffer/CakePHP --ignore=app/Config/Migration/,app/Config/database.php,app/Config/Schema/schema.php,$IGNORE_PLUGINS app || exit $?
-phpmd app text /etc/phpmd/rules.xml --exclude $TRAVIS_BUILD_DIR/app/Plugin,$TRAVIS_BUILD_DIR/app/Config/Migration,$TRAVIS_BUILD_DIR/app/Config/Schema/schema.php,$IGNORE_PLUGINS || exit $?
+#phpmd app text /etc/phpmd/rules.xml --exclude $TRAVIS_BUILD_DIR/app/Plugin,$TRAVIS_BUILD_DIR/app/Config/Migration,$TRAVIS_BUILD_DIR/app/Config/Schema/schema.php,$IGNORE_PLUGINS || exit $?
+phpmd app text tools/phpmd/rules.xml --exclude $TRAVIS_BUILD_DIR/app/Plugin,$TRAVIS_BUILD_DIR/app/Config/Migration,$TRAVIS_BUILD_DIR/app/Config/Schema/schema.php,$IGNORE_PLUGINS || exit $?
 phpcpd --exclude Test --exclude TestSuite --exclude Config --exclude Vendor $IGNORE_PLUGINS_OPTS app
 
 # js
