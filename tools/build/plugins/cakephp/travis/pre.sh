8f51a50eb2dbe4a7d2f5829780038b12cd55dae7
中島 正平 <nakajimashouhei@gmail.com>
2017-05-23 14:35:10 +0900

    Travisでエラーが出たため
    https://travis-ci.org/NetCommons3/Frames/jobs/235098073#L215

--- tools/build/plugins/cakephp/travis/pre.sh
+++ tools/build/plugins/cakephp/travis/pre.sh
@@ -72,6 +72,7 @@ mkdir -p build/cov
 mkdir -p tools/phpmd
 
 #sudo pip install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz
+pip install --upgrade pip
 pip install --user six
 pip install --user https://github.com/google/closure-linter/archive/v2.3.19.tar.gz
 
