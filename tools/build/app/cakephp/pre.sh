bbcaf03e04cf113b2d36474f02d2fd7703055227
Shohei Nakajima <nakajimashouhei@gmail.com>
2018-01-13 17:10:58 +0900

    PHPのバージョンでphpunitの切り替える

--- tools/build/app/cakephp/pre.sh
+++ tools/build/app/cakephp/pre.sh
@@ -10,7 +10,7 @@
 #fi
 
 if [ "$DB" = 'mysql' ]; then 
-	mysql -utest -ptest -e 'DROP DATABASE IF EXISTS test_nc3;
+		mysql -utest -ptest -e 'DROP DATABASE IF EXISTS test_nc3;
 	CREATE DATABASE test_nc3;'; 
 fi
 if [ "$DB" = 'postgresql' ]; then
@@ -26,8 +26,20 @@ chmod -R 777 app/tmp
 mkdir -p build/logs
 rm composer.lock
 
+# Fixing composer error
+# @link https://travis-ci.org/NetCommons3/NetCommons3/jobs/309361898#L466-L504
+# @link https://travis-ci.org/NetCommons3/NetCommons3/jobs/309361900#L510-L518
+PHP_VERSION="`php --version`"
+check54=`echo $PHP_VERSION | grep "^PHP 5.4"`
+check55=`echo $PHP_VERSION | grep "^PHP 5.5"`
+check56=`echo $PHP_VERSION | grep "^PHP 5.6"`
+if [ ! "$check54" = "" or  ! "$check55" = "" ]; then
+	composer require --dev --no-update phpunit/phpunit:~4.0@stable
+elif [ ! "$check56" = "" ]; then
+	composer require --dev --no-update phpunit/phpunit:~5.7@stable
+fi
+
 composer self-update
-composer config minimum-stability dev
 composer update
 
 cp app/Config/database.php.$DB app/Config/database.php
