ae7b9f7d67370e54230cb598632889d3461406e4
Mitsuru Mutaguchi <akagane99@gmail.com>
2019-02-20 17:31:54 +0900

    fix: phpバージョンに合わせたcoveralls実行

--- tools/build/plugins/cakephp/travis/post.sh
+++ tools/build/plugins/cakephp/travis/post.sh
@@ -42,5 +42,12 @@ if [ "$TRAVIS_BRANCH" = "master" -a "$DB" = "mysql" ]; then
 fi
 
 # coveralls
+# @link https://github.com/NetCommons3/NetCommons3/blob/0d675cf01786a79ed9c3a5faeb198b808686e3f2/tools/build/app/cakephp/travis/pre.sh#L10
+PHP_VERSION="`php --version`"
+check54=`echo $PHP_VERSION | grep "^PHP 5.4"`
 cd $NETCOMMONS_BUILD_DIR
-php vendors/bin/php-coveralls --root_dir . -vvv || exit $?
+if [ ! "$check54" = "" ]; then
+	php vendors/bin/coveralls --root_dir . -vvv || exit $?
+else
+	php vendors/bin/php-coveralls --root_dir . -vvv || exit $?
+fi
