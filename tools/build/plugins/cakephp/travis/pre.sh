7677da6e1494588f172e89ffc3bc0de91eae1842
Shohei Nakajima <nakajimashouhei@gmail.com>
2021-04-19 13:44:06 +0900

    Update pre.sh

--- tools/build/plugins/cakephp/travis/pre.sh
+++ tools/build/plugins/cakephp/travis/pre.sh
@@ -91,7 +91,7 @@ mkdir -p build/cov
 mkdir -p tools/phpmd
 
 #sudo pip install http://closure-linter.googlecode.com/files/closure_linter-latest.tar.gz
-#pip install --upgrade pip
+pip install --upgrade pip
 pip install --user six
 pip install --user https://github.com/NetCommons3/NetCommons3/raw/master/tools/build/plugins/cakephp/travis/v2.3.19.tar.gz
 
